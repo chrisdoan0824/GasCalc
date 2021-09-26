8dd96208a7ed05bd9236e89849f154e2a102a6eb
chrisdoan <doantu@mail.uc.edu>
2021-09-26 19:19:22 -0400

    Initial Commit

---
+++ src/test/java/com/gascalc/gascalc/GasCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gascalc.gascalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GasCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
