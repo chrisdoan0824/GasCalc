a8ffbf4e552d91cc62f383c8a8f60b2d6d3f1c28
chrisdoan <doantu@mail.uc.edu>
2021-10-21 01:43:13 -0400

    Create GasCalcController.java
    Added a controller

---
+++ src/main/java/com/gascalc/GasCalcController.java
@@ -0,0 +1,28 @@
+package com.gascalc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.gascalc.model.GasCalcModel;
+
+@Controller
+
+public class GasCalcController {
+
+	@RequestMapping("/search")
+	public String searchForm(Model model) {
+		GasCalcModel carSearch = new GasCalcModel();
+		model.addAttribute("carSearch", carSearch);
+		
+		return "search";
+	}
+	
+	
+	@PostMapping("/search")
+	public String searchResult(@ModelAttribute("carSearch") GasCalcModel model) {
+		return "search-result";
+	}
+}
