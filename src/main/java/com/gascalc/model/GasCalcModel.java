dbae81c133e2aabb672bd078c686f56acee12d00
Austin Walder <walderaj@mail.uc.edu>
2021-12-01 20:06:13 -0500

    Get Vehicles/list to display

--- src/main/java/com/gascalc/model/GasCalcModel.java
+++ src/main/java/com/gascalc/model/GasCalcModel.java
@@ -1,15 +1,18 @@
 package com.gascalc.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="car")
 public class GasCalcModel {
 
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	
 	@Column(name="id")
 	private int id;
 	@Column(name="car_make")
